19f0ac8aa44ba75e035d7adc880079ac3a27607a
kevinnail <fumalicious@gmail.com>
2022-09-12 16:59:12 -0700

    shell reveal class removed

--- app.js
+++ app.js
@@ -2,15 +2,39 @@
 // import { getRandomItem } from './utils.js';
 
 /* State */
+const gameState = 'guess';
 
 /* Actions */
-function loadPage() {}
+function loadPage() {
+    displayShells();
+}
 
 /* Components */
-
 /* Component */
 // get DOM
+const shellOne = document.getElementById('shell-1');
+const shellTwo = document.getElementById('shell-2');
+const shellThree = document.getElementById('shell-3');
+const pearlOne = document.getElementById('pearl-1');
+const pearlTwo = document.getElementById('pearl-2');
+const pearlThree = document.getElementById('pearl-3');
+
+const guessDiv = document.getElementById('guesses');
+const resultDiv = document.getElementById('results');
+const playAgainBtn = document.getElementById('play-again-button');
+
 // display
+function displayShells() {
+    console.log('running');
+    if (gameState === 'guess') {
+        shellOne.classList.remove('reveal');
+        shellTwo.classList.remove('reveal');
+        shellThree.classList.remove('reveal');
+        resultDiv.classList.add('hidden');
+    } else {
+    }
+}
+
 // event listeners
 
 /* Run page load code */
