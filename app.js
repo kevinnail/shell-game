406b2f65749170d5a82d9ddcfff6793e6e3f556a
kevinnail <fumalicious@gmail.com>
2022-09-12 18:57:20 -0700

    added nested if statements, event listeners, const for guess- event listeners not working yet but if statements do with tests

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ function loadPage() {
 
 /* Components */
 /* Component */
+const guess = 'guess-3';
 // get DOM
 const shellOne = document.getElementById('shell-1');
 const shellTwo = document.getElementById('shell-2');
@@ -22,20 +23,53 @@ const pearlThree = document.getElementById('pearl-3');
 const guessDiv = document.getElementById('guesses');
 const resultDiv = document.getElementById('results');
 const playAgainBtn = document.getElementById('play-again-button');
-
+const guessOne = document.getElementById('guess-1');
+const guessTwo = document.getElementById('guess-2');
+const guessThree = document.getElementById('guess-3');
+const playAgain = document.getElementById('play-again-button');
+const winsDisplay = document.getElementById('wins-display');
+const lossDisplay = document.getElementById('losses-display');
+const totalDisplay = document.getElementById('total-display');
 // display
 function displayShells() {
-    console.log('running');
     if (gameState === 'guess') {
         shellOne.classList.remove('reveal');
         shellTwo.classList.remove('reveal');
         shellThree.classList.remove('reveal');
         resultDiv.classList.add('hidden');
-    } else {
+    } else if (gameState === 'results') {
+        if (guess === 'guess-1') {
+            shellOne.classList.add('reveal');
+            pearlOne.classList.remove('hidden');
+        }
+        if (guess === 'guess-2') {
+            shellTwo.classList.add('reveal');
+            pearlThree.classList.remove('hidden');
+        }
+        if (guess === 'guess-3') {
+            shellThree.classList.add('reveal');
+            pearlThree.classList.remove('hidden');
+        } else {
+            guessDiv.classList.add('hidden');
+        }
     }
 }
-
 // event listeners
+guessOne.addEventListener('click', () => {
+    guess = 'guess-1';
+});
+guessTwo.addEventListener('click', () => {
+    guess = 'guess-2';
+});
+guessThree.addEventListener('click', () => {
+    guess = 'guess-3';
+});
 
 /* Run page load code */
 loadPage();
+{
+    shellOne.classList.remove('reveal');
+    shellTwo.classList.remove('reveal');
+    shellThree.classList.remove('reveal');
+    resultDiv.classList.add('hidden');
+}
